3cf1144d92a29b38cdbb0883f495fa4cc3899016
onukwilip <onukwilip@gmail.com>
2024-12-15 08:42:16 +0100

    Disabled caching of data during the Next Js build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import TodoSection from "./_components/TodoSection.component";
 import client from "./config/contentful-client.config";
 import { TTodoItem } from "../../types";
 import { EntrySkeletonType } from "contentful";
+import { Metadata } from "next";
 
+export const metadata = {
+  revalidate: 0, // Time in seconds to revalidate the cache
+};
 export default async function Home() {
   try {
     const todo_response = await client.getEntries<
